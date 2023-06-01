a53ba183dae4fa2dea269bd179969ff73a6b0c29
梅雨 <yuri.tsuchikawa@gmail.com>
2023-06-02 05:49:10 +0900

    Add login form

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,6 +1,23 @@
+import React from "react"
+import styled from "styled-components"
+import Button from "../components/Button"
+import Input from "../components/Input"
+
+const StyledLoginForm = styled(({ className }) => 
+	<div className={className}>
+		<Input placeholder="ユーザー名"/>
+		<Input placeholder="パスワード"/>
+		<Button text="登録" />
+	</div>
+)`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+`
+
 const Login = () => {
 	return(
-		<p>Login画面</p>
+		<StyledLoginForm />
 	)
 }
 
